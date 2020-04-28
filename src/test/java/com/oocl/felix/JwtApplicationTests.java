6fed6c89e937c8f3960c78a2c27887f33e7aa650
Felix <742863722@qq.com>
2020-04-28 23:44:54 +0800

    init project

---
+++ src/test/java/com/oocl/felix/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oocl.felix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
