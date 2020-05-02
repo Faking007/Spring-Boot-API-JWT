435094ce2649680523a793e264a31467dc7376dc
Felix <742863722@qq.com>
2020-05-02 17:36:10 +0800

    Use jwt instead of jwt to avoid UnsupportedJwtException

--- src/main/java/com/oocl/felix/jwt/util/JwtTokenUtil.java
+++ src/main/java/com/oocl/felix/jwt/util/JwtTokenUtil.java
@@ -65,7 +65,7 @@ public class JwtTokenUtil implements Serializable {
     }
 
     private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJwt(token).getBody();
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
     }
 
 }
